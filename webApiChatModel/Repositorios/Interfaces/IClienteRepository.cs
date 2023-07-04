36c07c37cdca99b837b8e0e496cb0cd9eb452148
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-07-04 14:40:54 -0300

    Criação da interface IClienteRepository

---
+++ webApiChatModel/Repositorios/Interfaces/IClienteRepository.cs
@@ -0,0 +1,10 @@
+﻿using webApiChatModel.Models;
+
+namespace webApiChatModel.Repositorios.Interfaces
+{
+    public interface IClienteRepository
+    {
+        Task<List<ClienteModel>> ListarTodosClientes();
+        Task<ClienteModel> ObterClientePorCPF(long cpf);
+    }
+}

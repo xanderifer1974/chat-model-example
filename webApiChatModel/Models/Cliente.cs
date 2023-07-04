c277a7265f8377981503c247997714774fb93b37
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-07-04 11:49:22 -0300

    Criação da model cliente

---
+++ webApiChatModel/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿namespace webApiChatModel.Models
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public long Cpf { get; set; }
+        public string? Nome { get; set; }
+        public bool Ativo { get; set; }
+
+        public Cliente(int idCliente, long cpf, string? nome, bool ativo)
+        {
+            IdCliente = idCliente;
+            Cpf = cpf;
+            Nome = nome;
+            Ativo = ativo;
+        }
+
+        public Cliente()
+        {
+            
+        }
+    }
+}

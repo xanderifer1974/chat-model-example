8cb6562489ff6b2361e7ca2e10c9d8b11eb09c82
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-07-14 15:35:16 -0300

    Implementação de testes unitário ChatServicesTests

--- WebApiChatModelTest/ApiChatTest/Services/ChatServiceTests.cs
+++ WebApiChatModelTest/ApiChatTest/Services/ChatServiceTests.cs
@@ -49,22 +49,31 @@ namespace WebChatTest.ApiChatTest.Services
             Assert.Equal(chatModelEsperado, resultado);
         }
 
-       
-        //[Fact]
-        //public async Task BuscarConversaPorPergunta_DeveRetornarRespostaQuandoCPFValido()
-        //{
-        //    // Arrange
-        //    string pergunta = "12345678901"; // CPF válido
-        //    var clienteModel = new ClienteModel { Nome = "João" };
-        //    _clienteRepositorioMock.Setup(repo => repo.ObterClientePorCPF(It.IsAny<long>())).Returns(clienteModel);
-        //    var chatService = new ChatService(_chatRepositorioMock.Object, _clienteRepositorioMock.Object);
 
-        //    // Act
-        //    var resultado = await chatService.BuscarConversaPorPergunta(pergunta);
+        [Fact]
+        public async Task BuscarConversaPorPergunta_DeveRetornarRespostaQuandoCPFValido()
+        {
+            // Arrange
+            string pergunta = "21201141028"; // CPF válido
+            var clienteModel = new ClienteModel { Nome = "João" };
+            _clienteRepositorioMock.Setup(repo => repo.ObterClientePorCPF(It.IsAny<long>())).Returns(clienteModel);
+            var chatService = new ChatService(_chatRepositorioMock.Object, _clienteRepositorioMock.Object);
 
-        //    // Assert
-        //    Assert.NotNull(resultado);
-        //    Assert.Equal($"<h6>Olá {clienteModel.Nome}, escolha uma das opções abaixo.</h6>", resultado.Resposta);
-        //}
+            // Act
+            var resultado = await chatService.BuscarConversaPorPergunta(pergunta);
+            var resposta = $"<h6>Olá {clienteModel.Nome}, escolha uma das opções abaixo.</h6>" +
+                        $"<ul><li>01 – Alterar pacote de canais</li>" +
+                        $"<li>02 – Alterar dados cadastrais</li>" +
+                        $"<li>03 – Solicitar um novo ponto</li>" +
+                        $"<li>04 – Alterar endereço da assinatura</li>" +
+                        $"<li>05 – Solicitar segunda via da fatura</li>" +
+                        $"<li>06 – Renegociar dívida</li>" +
+                        $"<li>07 – Cancelar assinatura</li>" +
+                        $"<li>08 - Falar com um atendente</li></ul>";
+
+            // Assert
+            Assert.NotNull(resultado);
+            Assert.Equal(resposta, resultado.Resposta);
+        }
     }
 }
